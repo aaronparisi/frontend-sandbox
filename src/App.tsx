54bd501dd0e8ae24d1340f518661aef55c693dc0
aaronparisi <parisi.aaron@gmail.com>
2022-04-17 22:01:17 -0700

    added more projects, slight adjustment to nav bar styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import NavBar from "./components/NavBar/NavBar";
 import ProjectDisplay from "./components/ProjectDisplay/ProjectDisplay";
 import "./stylesheets/index.css";
 
-const projects = ["https://www.faq.aaronparisi.dev"];
+const projects = [
+  "https://www.faq.aaronparisi.dev",
+  "https://www.archer.aaronparisi.dev",
+  "https://www.towers.aaronparisi.dev",
+  "https://www.jeerkat-js.aaronpari.dev",
+  "https://www.v1.aaronparisi.dev",
+  "https://www.tictactoe.aaronparisi.dev",
+];
 
 function App() {
-  const [selectedProject, setSelectedProject] = useState<string>("");
+  const [selectedProject, setSelectedProject] = useState<string>(projects[0]);
 
   return (
     <div className="App">
-      <NavBar projects={projects} setSelectedProject={setSelectedProject} />
+      <NavBar
+        projects={projects}
+        selectedProject={selectedProject}
+        setSelectedProject={setSelectedProject}
+      />
       <ProjectDisplay project={selectedProject} />
     </div>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,11 +3,13 @@ import React, { Dispatch, SetStateAction } from "react";
 interface NavBarProps {
   projects: string[];
   setSelectedProject: Dispatch<SetStateAction<string>>;
+  selectedProject: string;
 }
 
 const NavBar: React.FC<NavBarProps> = ({
   projects,
   setSelectedProject,
+  selectedProject,
 }): JSX.Element => {
   const projectDisplay = (project: string): string => {
     let words = project.split(".");
@@ -23,7 +25,15 @@ const NavBar: React.FC<NavBarProps> = ({
             key="project"
             onClick={() => setSelectedProject(project)}
           >
-            <div className="project-link-text">{projectDisplay(project)}</div>
+            <div
+              className={
+                selectedProject === project
+                  ? `project-link-text selected-project`
+                  : `project-link-text`
+              }
+            >
+              {projectDisplay(project)}
+            </div>
           </li>
         );
       })}
