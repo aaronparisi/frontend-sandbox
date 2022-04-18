08172c5605a393d767d92ab254523e23b25e4165
aaronparisi <parisi.aaron@gmail.com>
2022-04-17 19:42:59 -0700

    nav bar is now clickable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import ProjectDisplay from "./components/ProjectDisplay/ProjectDisplay";
 import "./stylesheets/index.css";
@@ -6,10 +6,12 @@ import "./stylesheets/index.css";
 const projects = ["https://www.faq.aaronparisi.dev"];
 
 function App() {
+  const [selectedProject, setSelectedProject] = useState<string>("");
+
   return (
     <div className="App">
-      <NavBar projects={projects} />
-      <ProjectDisplay project={projects[0]} />
+      <NavBar projects={projects} setSelectedProject={setSelectedProject} />
+      <ProjectDisplay project={selectedProject} />
     </div>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 interface NavBarProps {
   projects: string[];
+  setSelectedProject: Dispatch<SetStateAction<string>>;
 }
 
-const NavBar: React.FC<NavBarProps> = ({ projects }): JSX.Element => {
+const NavBar: React.FC<NavBarProps> = ({
+  projects,
+  setSelectedProject,
+}): JSX.Element => {
   const projectDisplay = (project: string): string => {
     let words = project.split(".");
     return words[1];
@@ -14,7 +18,11 @@ const NavBar: React.FC<NavBarProps> = ({ projects }): JSX.Element => {
     <ul className="nav-bar">
       {projects.map((project: string) => {
         return (
-          <li className="project-link" key="project">
+          <li
+            className="project-link"
+            key="project"
+            onClick={() => setSelectedProject(project)}
+          >
             <div className="project-link-text">{projectDisplay(project)}</div>
           </li>
         );
